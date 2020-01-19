59824722007d66a8c6eed6fbfc7ad6c94b74c5e1
ghostops <ludvig@ghostops.nu>
2020-01-19 12:01:17 +0000

    script is MVP

--- generate.py
+++ generate.py
@@ -1,11 +1,15 @@
+import time
 import datetime
 import os
 import boto3
+import requests
 from wand.image import Image
 from wand.image import Color
 from wand.font import Font
+from bs4 import BeautifulSoup
 
 FONT = Font(path="./font.ttf", color=Color("#ffffff"))
+# Script should run on mondays
 WEEK = datetime.date.today().isocalendar()[1]
 
 def set_text(img, txt, top):
@@ -19,21 +23,21 @@ def set_text(img, txt, top):
         gravity='west'
     )
 
-def set_up_text():
+def set_up_text(last_week, today, diff):
     img = Image(filename="./img/up.png")
 
-    set_text(img, "17:32", 192)
-    set_text(img, "18:32", 218)
-    set_text(img, "19:32", 244)
+    set_text(img, today, 192)
+    set_text(img, last_week, 218)
+    set_text(img, diff, 244)
 
     img.save(filename="generated/up.png")
 
-def set_down_text():
+def set_down_text(last_week, today, diff):
     img = Image(filename="./img/down.png")
 
-    set_text(img, "17:32", 56)
-    set_text(img, "18:32", 82)
-    set_text(img, "19:32", 108)
+    set_text(img, today, 56)
+    set_text(img, last_week, 82)
+    set_text(img, diff, 108)
 
     img.save(filename="generated/down.png")
 
@@ -66,13 +70,93 @@ def render_video():
 def upload_video():
     s3 = boto3.client('s3')
     with open("./generated/output.mp4", "rb") as f:
-        s3.upload_fileobj(f, "solfilm", "week-{}.mp4".format(WEEK))
+        s3.upload_fileobj(f, "solfilm", "week-{}.mp4".format(WEEK), ExtraArgs={'ACL':'public-read'})
 
-# set_up_text()
-# set_down_text()
+def scrape_data():
+    today = datetime.datetime.today()
+    last_week = today - datetime.timedelta(days=7)
+
+    # if both year and month is same we only make one request
+    same = (today.month == last_week.month) and (today.year == last_week.year)
+
+    base = "https://www.timeanddate.com/sun/iceland/reykjavik?month={}&year={}"
+
+    def scrape(month, year):
+        url = base.format(month, year)
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        def extract(class_):
+            result = soup.find_all('td', class_=class_)
+
+            res = []
+
+            for job_elem in result:
+                if len(str(job_elem)) > 99:
+                    res.append(job_elem.text[:5])
+
+            return res
+
+
+        sunrises = extract('c sep')
+        sunsets = extract('sep c')
+
+        return { 'sunrises': sunrises, 'sunsets': sunsets }
+
+    months = {}
+
+    months[last_week.month] = scrape(last_week.month, last_week.year)
+
+    if not same:
+        months[today.month] = scrape(today.month, today.year)
+
+    todays_sunrise = months[today.month]['sunrises'][today.day - 1]
+    todays_sunset = months[today.month]['sunsets'][today.day - 1]
+
+    last_week_sunrise = months[today.month]['sunrises'][last_week.day - 1]
+    last_week_sunset = months[today.month]['sunsets'][last_week.day - 1]
+
+    return {
+        'last_week_sunrise': last_week_sunrise,
+        'last_week_sunset': last_week_sunset,
+        'todays_sunrise': todays_sunrise,
+        'todays_sunset': todays_sunset
+    }
+
+def calc_diff(last_week, today):
+    lw = datetime.datetime.strptime(last_week, '%H:%M')
+    t = datetime.datetime.strptime(today, '%H:%M')
+    delta_min = abs((t - lw).total_seconds() / 60)
+
+    if delta_min == 0:
+        return '-'
+
+    prefix = '+'
+
+    # this is a funny hack
+    if datetime.datetime.now().month > 7:
+        prefix = '-'
+
+    stringified = str(int(delta_min))
+
+    return "{}{}".format(prefix, stringified)
+
+data = scrape_data()
+
+set_up_text(
+    data['last_week_sunrise'],
+    data['todays_sunrise'],
+    calc_diff(data['last_week_sunrise'], data['todays_sunrise'])
+)
+set_down_text(
+    data['last_week_sunset'],
+    data['todays_sunset'],
+    calc_diff(data['last_week_sunset'], data['todays_sunset'])
+)
 set_intro_text()
 
-# render_video()
+render_video()
 
 upload_video()
 
